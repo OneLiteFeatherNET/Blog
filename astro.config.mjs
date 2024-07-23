3d42b0a16f1f38390c03ff30aa35b2f150081eed
Phillipp Glanz <p.glanz@madfix.me>
2024-07-23 10:57:38 +0200

    Try to add darkmode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.onelitefeather.net',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), preact(), tailwind()],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "de"],
