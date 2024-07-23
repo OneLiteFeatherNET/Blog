4c8fe08a7c5693fcf5606d141742072bd4658322
Phillipp Glanz <p.glanz@madfix.me>
2024-07-23 12:04:29 +0200

    Add google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
+
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.onelitefeather.net',
-  integrations: [mdx(), sitemap(), preact(), tailwind()],
+  integrations: [mdx(), sitemap(), preact(), tailwind(),partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"]
