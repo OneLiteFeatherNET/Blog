247c703df9c3bcd2b610a6bba7d57ca7b13236ec
Phillipp Glanz <p.glanz@madfix.me>
2024-07-23 11:17:52 +0200

    Add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import sitemap from '@astrojs/sitemap';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.onelitefeather.net',
-	integrations: [mdx(), sitemap(), preact(), tailwind()],
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "de"],
-	}
+  site: 'https://blog.onelitefeather.net',
+  integrations: [mdx(), sitemap(), preact(), tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"]
+  },
+  output: "server",
+  adapter: cloudflare()
 });
