c7cf093d189865fc625d0dfe6431a261eb0b3b62
Phillipp Glanz <p.glanz@madfix.me>
2025-02-05 16:01:37 +0100

    Improve sitemap and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,15 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.onelitefeather.net',
-  integrations: [mdx(), sitemap(), preact(), robotsTxt(), partytown({
+  integrations: [mdx(), sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        de: 'de-DE',
+      },
+    },
+  }), preact(), robotsTxt(), partytown({
     config: {
       forward: ["dataLayer.push"]
     }
